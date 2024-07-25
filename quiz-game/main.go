630f4bbc99bde932eb2eba9c56ea9ac7529d9b97
twill <twill1@gmx.com>
2024-07-25 17:20:03 -0500

    Fix bug and add prompt to begin

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -17,18 +17,33 @@ type ProblemSolution struct {
 
 func main() {
 	fileName := flag.String("fileName", "problems.csv", "Specify the name of the input CSV, the default is problems.csv")
-	// timer := flag.Int("timer", 30, "The time limit for each question")
+	timer := flag.Int("timer", 30, "The time limit for each question")
 	// shuffle := flag.Bool("shuffle", false, "Boolean value to shuffle the quiz questions")
 
-	problemSolutionList := processFile(*fileName)
+	begin := begin(*timer)
 
-	totalQuestions := len(problemSolutionList)
-	correctAnswers := 0
+	if begin {
+		problemSolutionList := processFile(*fileName)
 
-	quizUser(problemSolutionList, &correctAnswers)
+		totalQuestions := len(problemSolutionList)
+		correctAnswers := 0
 
-	fmt.Printf("There were %+v total questions\n", totalQuestions)
-	fmt.Printf("You answered %+v correctly\n", correctAnswers)
+		quizUser(problemSolutionList, &correctAnswers)
+
+		fmt.Printf("There were %+v total questions\n", totalQuestions)
+		fmt.Printf("You answered %+v correctly\n", correctAnswers)
+	} else {
+		fmt.Print("Exiting program...\n")
+	}
+}
+
+func begin(time int) bool {
+	fmt.Printf("Please type `go` to start the quiz, you will have: %v seconds\n", time)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+
+	return input == "go"
 }
 
 func quizUser(problemSolutionList []ProblemSolution, correctAnswers *int) {
@@ -64,18 +79,16 @@ func processFile(fileName string) []ProblemSolution {
 
 	var problemSolutionList []ProblemSolution
 
-	for i, line := range data {
-		if i > 0 {
-			var record ProblemSolution
-			for j, field := range line {
-				if j == 0 {
-					record.Problem = field
-				} else if j == 1 {
-					record.Solution = field
-				}
+	for _, line := range data {
+		var record ProblemSolution
+		for j, field := range line {
+			if j == 0 {
+				record.Problem = field
+			} else if j == 1 {
+				record.Solution = field
 			}
-			problemSolutionList = append(problemSolutionList, record)
 		}
+		problemSolutionList = append(problemSolutionList, record)
 	}
 
 	return problemSolutionList
