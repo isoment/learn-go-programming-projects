302ae8a2d281d2c4cdb748cdedbab58c27baa9ab
twill <twill1@gmx.com>
2024-07-26 09:28:24 -0500

    Refactor to parseFlags function

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -18,52 +18,62 @@ type ProblemSolution struct {
 }
 
 func main() {
+	fileName, timeLimit, shuffle := parseFlags()
+
+	fmt.Printf("Suffle: %v\n", shuffle)
+
+	problemSolutionList := processFile(fileName)
+
+	promptUserToBegin(timeLimit)
+
+	totalQuestions := len(problemSolutionList)
+	correctAnswers := 0
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	t := time.NewTimer(time.Duration(timeLimit) * time.Second)
+	done := make(chan bool)
+
+	go quizUser(ctx, problemSolutionList, &correctAnswers, done)
+
+	select {
+	case <-t.C:
+		fmt.Println("\nTime's up!")
+		cancel()
+	case <-done:
+		fmt.Println("\nQuiz completed!")
+	}
+
+	fmt.Printf("There were %+v total questions\n", totalQuestions)
+	fmt.Printf("You answered %+v correctly\n", correctAnswers)
+}
+
+/*
+Parse the command flags and return their values
+*/
+func parseFlags() (string, int, bool) {
 	fileName := flag.String("fileName", "problems.csv", "Specify the name of the input CSV, the default is problems.csv")
 	timeLimit := flag.Int("timeLimit", 30, "The time limit for each question")
-	// shuffle := flag.Bool("shuffle", false, "Boolean value to shuffle the quiz questions")
+	shuffle := flag.Bool("shuffle", false, "Boolean value to shuffle the quiz questions")
 	flag.Parse()
 
-	problemSolutionList := processFile(*fileName)
-
-	begin := begin(*timeLimit)
-
-	if begin {
-		totalQuestions := len(problemSolutionList)
-		correctAnswers := 0
-
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		t := time.NewTimer(time.Duration(*timeLimit) * time.Second)
-		done := make(chan bool)
-
-		go quizUser(ctx, problemSolutionList, &correctAnswers, done)
-
-		select {
-		case <-t.C:
-			fmt.Println("\nTime's up!")
-			cancel()
-		case <-done:
-			fmt.Println("\nQuiz completed!")
-		}
-
-		fmt.Printf("There were %+v total questions\n", totalQuestions)
-		fmt.Printf("You answered %+v correctly\n", correctAnswers)
-	} else {
-		fmt.Print("Exiting program...\n")
-	}
+	return *fileName, *timeLimit, *shuffle
 }
 
 /*
 Prompt the user to begin the game
 */
-func begin(time int) bool {
+func promptUserToBegin(time int) {
 	fmt.Printf("Please type 'go' to start the quiz, you will have: %v seconds\n", time)
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	input := scanner.Text()
 
-	return input == "go"
+	if input != "go" {
+		fmt.Print("Exiting program...\n")
+		os.Exit(1)
+	}
 }
 
 /*
