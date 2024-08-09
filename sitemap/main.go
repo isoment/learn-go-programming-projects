6313b669c51bab2c92d8be8aa2e0a20573f3c1e5
twill <twill1@gmx.com>
2024-08-09 14:30:35 -0500

    GET request

--- sitemap/main.go
+++ sitemap/main.go
@@ -3,11 +3,33 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
 	urlFlag := flag.String("urlFlag", "https://gophercises.com", "URL to build a sitemap for")
 	flag.Parse()
 
-	fmt.Println(urlFlag)
+	fmt.Println(*urlFlag)
+
+	/*
+		- Make a GET request to fetch the webpage
+		- Parse links
+		- Build correct URLs from links, add domain
+		- Remove links to external domains
+		- Find all the pages (BFS), repeat the above steps for each page
+		- Export the sitemap to an XML file
+	*/
+
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	// We can output the response body to the terminal
+	io.Copy(os.Stdout, resp.Body)
 }
