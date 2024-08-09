4890e060f40f656c7fb3d06401cc191d000b30e1
twill <twill1@gmx.com>
2024-08-09 16:40:30 -0500

    Extract full URLs from links

--- sitemap/main.go
+++ sitemap/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
+
+	"github.com/isoment/sitemap/pkg/link"
 )
 
 func main() {
@@ -31,5 +33,30 @@ func main() {
 	defer resp.Body.Close()
 
 	// We can output the response body to the terminal
-	io.Copy(os.Stdout, resp.Body)
+	// io.Copy(os.Stdout, resp.Body)
+
+	reqURL := resp.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
+	}
+	base := baseURL.String()
+
+	links, _ := link.Parse(resp.Body)
+
+	var hrefs []string
+
+	// Filter the original links and get the full URLs
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 }
