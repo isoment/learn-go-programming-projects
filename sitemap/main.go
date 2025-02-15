3c0ac2a0048e5fbd6df27eb3245069fc63c547c7
twill <twill1@gmx.com>
2025-02-15 14:10:39 -0600

    BFS with depth for traversing pages for links

--- sitemap/main.go
+++ sitemap/main.go
@@ -12,10 +12,9 @@ import (
 )
 
 func main() {
-	urlFlag := flag.String("urlFlag", "https://gophercises.com", "URL to build a sitemap for")
+	urlFlag := flag.String("url", "https://gophercises.com", "URL to build a sitemap for")
+	maxDepth := flag.Int("depth", 3, "The maximum link depth to traverse")
 	flag.Parse()
-
-	fmt.Println(*urlFlag)
 
 	/*
 		- Make a GET request to fetch the webpage
@@ -26,17 +25,69 @@ func main() {
 		- Export the sitemap to an XML file
 	*/
 
-	urls := get(*urlFlag)
+	urls := bfs(*urlFlag, *maxDepth)
 
-	for _, href := range urls {
-		fmt.Println(href)
+	for _, url := range urls {
+		fmt.Println(url)
 	}
+}
+
+// We can define a type for an empty struct, to instantiate it we can use empty{}. This is more memory
+// efficient than assigning a bool or int for a map value.
+type empty struct{}
+
+// A BFS implementation for discovering links for a given url
+func bfs(urlStr string, maxDepth int) []string {
+	// Keep track of the urls we have seen. We can use an empty struct for the value since we
+	// have nothing that we need to put here, only the keys are important.
+	seen := make(map[string]empty)
+
+	// A queue where the key is the url of all the links we will need to call the get() method on. This is
+	// the current level of the links we are working on. The child links get added to nq
+	var q map[string]empty
+
+	// All the links that we have not seen yet will go in nq (next queue). When q is empty nq will be
+	// assigned to q. We want to set the urlStr right away to the base url that bfs was called with.
+	nq := map[string]empty{
+		urlStr: empty{},
+	}
+
+	for i := 0; i <= maxDepth; i++ {
+		// Move nq to q and create an empty map and assign to q
+		q, nq = nq, make(map[string]empty)
+
+		// Iterate over the q map
+		for url, _ := range q {
+			// Check if the url value is already in the seen map, if it is ok will be true and we want
+			// to skip
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			// If the url has not been seen mark it as seen
+			seen[url] = empty{}
+			// Iterate over the links we get back from the get() method
+			for _, link := range get(url) {
+				nq[link] = empty{}
+			}
+		}
+	}
+
+	// This will be a slice of all the urls all the seen urls
+	ret := make([]string, 0, len(seen))
+
+	// Iterate over the seen map and built up the return slice
+	for url, _ := range seen {
+		ret = append(ret, url)
+	}
+
+	return ret
 }
 
 func get(urlStr string) []string {
 	resp, err := http.Get(urlStr)
+
 	if err != nil {
-		panic(err)
+		return []string{}
 	}
 
 	defer resp.Body.Close()
@@ -44,10 +95,13 @@ func get(urlStr string) []string {
 	// We can output the response body to the terminal
 	// io.Copy(os.Stdout, resp.Body)
 
+	// Get the request url domain from the response
 	reqURL := resp.Request.URL
 	baseURL := &url.URL{
+		// http, https, ftp etc
 		Scheme: reqURL.Scheme,
-		Host:   reqURL.Host,
+		// Host domain
+		Host: reqURL.Host,
 	}
 	base := baseURL.String()
 
